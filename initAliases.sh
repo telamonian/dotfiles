2b310a217ab5d7e137e8f42ade978ab0673f8e39
telamonian <mklein@jhu.edu>
2017-08-04 00:09:16 -0400

    Added .gitignore_global (with some OS X cruft patterns) to dotfile.d

--- initAliases.sh
+++ initAliases.sh
@@ -48,7 +48,7 @@ absdir=$(cd $(dirname "${reldir}") && pwd -P)/$(basename "${reldir}")
 # might be a better way of doing this, but for now we just cheat with pyton in order to get the relative path between the dotfiles (including this script) and ${HOME}
 relpath=$(python -c "import os.path; print os.path.relpath(\"${absdir}\", \"${HOME}\")")
 
-## loop through the array of dotfiles and create softlinks in ${HOME} pointing to them along relpath
+## loop through every file in dotfile.d that starts with exactly one period and create softlinks in ${HOME} pointing to them along relpath
 for dotfileReal in ${absdir}/dotfile.d/.[!.]*; do
     dotfileName=$(basename $dotfileReal)
     linkPath=${HOME}/$dotfileName
